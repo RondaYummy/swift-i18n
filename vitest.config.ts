55ccccfad27aeefc2a6a77d711677266e8d2e6bb
RondoYummy <basrers@ukr.net>
2025-08-23 19:29:25 +0000

    chore(release): bump version to 1.2.5; update homepage URL and improve Vitest configuration exclusions

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, configDefaults } from "vitest/config";
 
 export default defineConfig({
   test: {
@@ -8,20 +8,10 @@ export default defineConfig({
       provider: "v8",
       reporter: ["text", "html", "json-summary", "lcov"],
       exclude: [
-        "coverage/**",
-        "dist/**",
-        "packages/*/test{,s}/**",
-        "**/*.d.ts",
-        "cypress/**",
-        "test{,s}/**",
-        "test{,-*}.{js,cjs,mjs,ts,tsx,jsx}",
-        "**/*{.,-}test.{js,cjs,mjs,ts,tsx,jsx}",
-        "**/*{.,-}spec.{js,cjs,mjs,ts,tsx,jsx}",
-        "**/__tests__/**",
-        "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
-        "**/.{eslint,mocha,prettier}rc.{js,cjs,yml}",
+        ...configDefaults.exclude,
         "src/types/**",
         "src/index.ts",
+        "docs/**"
       ],
     },
   },
